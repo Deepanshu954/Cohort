a3a1a57cbd7c228d1b55c7417f9d920e02fba08d
Deepanshu954 <deepanshu95488@gmail.com>
2025-07-04 15:45:34 +0530

    .

--- 14/TypeScript/src/index.ts
+++ 14/TypeScript/src/index.ts
@@ -1,4 +1,15 @@
-const greet = (name: string) => `Hello, ${ name }!`;
+function greet(user : {
+    name : string,
+    age : number
+})
+{
+    console.log("Nice to meet you, "+ user.name);
+    console.log(`It's been ${user.age} since i know you`);
+};
 
+let user = {
+    name : "Deepanshu",
+    age : 21,
+};
 
-
+greet(user);
