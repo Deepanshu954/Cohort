c09dc1fc22c66c92dfa53e1482b362ad800bcd5f
Deepanshu954 <deepanshu95488@gmail.com>
2025-07-04 11:56:45 +0530

    update

--- 14/14.1/src/App.tsx
+++ 14/14.1/src/App.tsx
@@ -4,25 +4,9 @@ function App() {
 
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
+    <div className="App flex bg:green-100">
+      Testing Phase
+    </div>
   )
 }
 
