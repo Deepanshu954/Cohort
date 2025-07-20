d0fa00ff3df966a8e191184cbd7fc283755e8052
Deepanshu954 <deepanshu95488@gmail.com>
2025-07-20 10:58:24 +0530

    day 16

---
+++ 16/BackEnd/src/index.ts
@@ -0,0 +1,18 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port : 8080 });
+
+// Event Handler
+wss.on("connection", function(socket) {
+    console.log("User Connected")
+    
+    socket.on("message", (e) =>
+    {
+        if(e.toString() === "ping")
+        {
+            socket.send("pong");
+        }
+    })
+    
+
+})
---
+++ 16/FrontEnd/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css"
+function App()
+{
+  return (
+    <div>
+      <div>
+        <input type ="text" placeholder="Message..." ></input>
+      </div>
+    </div>
+  )
+}
+
+export default App
