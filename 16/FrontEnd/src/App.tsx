821ba9d0ddd156bc96bbe13d5c78930261a8a1c0
Deepanshu954 <deepanshu95488@gmail.com>
2025-07-20 11:01:31 +0530

    .

--- 16/FrontEnd/src/App.tsx
+++ 16/FrontEnd/src/App.tsx
@@ -1,10 +1,18 @@
 import "./App.css"
 function App()
 {
+  function SendMessage()
+  {
+
+  }
+
   return (
     <div>
       <div>
         <input type ="text" placeholder="Message..." ></input>
+      </div>
+      <div>
+        <button onClick = {SendMessage}> Send </button>
       </div>
     </div>
   )
