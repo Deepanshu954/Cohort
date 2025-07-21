6da898f67a0c507af5203c6b4564298382dd490d
Deepanshu954 <deepanshu95488@gmail.com>
2025-07-21 06:30:45 +0530

    .

--- 16/Chat App/FrontEnd/src/App.tsx
+++ 16/Chat App/FrontEnd/src/App.tsx
@@ -1,11 +1,40 @@
-import "./App.css"
+import "./App.css";
+import { useState, useEffect, useRef } from "react";
 
 function App()
 {
+  const [ socket, setSocket ] = useState();
+  const inputRef = useRef();
+
+  function SendMessage()
+  {
+    if(!socket)
+    {
+      return;
+    }
+    const message = inputRef.current.value;
+
+    // @ts-ignore
+    socket.send(message);
+  }
+
+  useEffect(() =>
+  {
+    const ws = new WebSocket("ws://localhost:8080");
+    setSocket(ws)
+
+    ws.onmessage = (ev) => {
+      <div>
+        {ev.data}
+      </div>
+    }
+  }, [])
+
   return (
     <>
       <div>
-        Hey There
+        <input ref = { inputRef } type = "text" placeholder = "Message..." />
+        <button onClick = {SendMessage}>Send</button>
       </div>
     </>
   )
